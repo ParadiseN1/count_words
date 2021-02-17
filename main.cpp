c9c56525b428afab279f21ed5c9e5046c107d2d9
ParadiseN <vovanpri337@gmail.com>
2021-02-17 15:18:09 +0200

    merge queue works sequential

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <unordered_map>
 #include <thread>
 #include <numeric>
+#include <algorithm>
 
 #include "src/lib_archive.h"
 #include "src/word_count.h"
@@ -16,9 +17,10 @@
 #include "src/indexed_file.h"
 #include "src/time.h"
 
-void print(std::string strz){
-    std::cout << strz << std::endl;
-}
+struct indexed_file{
+    std::string str;
+    u_int       idx;
+};
 
 namespace fs = std::filesystem;
 const int MAX_ARCHIVE_SIZE = 10000000;
@@ -26,26 +28,23 @@ std::condition_variable cv;
 std::mutex mu;
 queue_t<indexed_file> file_content_queue;
 u_int n_calc_threads = 10;
-u_int n_merge_threads = 4;
+u_int n_merge_threads = 10;
 bool readEnd = false;
+bool calcEnd = false;
 
-void calc_thread(std::vector<std::unordered_map<std::string, u_int>> *files_words, int thread_idx)
+void calc_thread(std::vector<std::unordered_map<std::string, u_int>> *files_words, int thread_idx, queue_t<std::unordered_map<std::string, u_int>> *merge_queue)
 {
     std::cout << "Thread {" << thread_idx << "} is alive." << std::endl;
     while(!readEnd || !file_content_queue.empty()) {
-        //std::unique_lock<std::mutex> lk(mu); //// replace CV to queue
         std::cout << "Thread {" << thread_idx << "} start waiting." << "readEnd:" << readEnd << ", queue.empty():" << file_content_queue.empty() << std::endl;
-        //cv.wait(lk, [] { return !file_content_queue.empty(); });
 
         indexed_file queueObj;
         std::unordered_map<std::string, u_int> word_map;
         file_content_queue.pop(queueObj);
 
-        //// Try unclock() mutex HERE
-        //lk.unlock();
         std::cout << "{" << thread_idx << "}[" << queueObj.idx << "]Calc thread Begin...  " << "readEnd:" << readEnd << ", queue.empty():" << file_content_queue.empty() <<  std::endl;
-//      std::cout << "idx:" << queueObj.idx << "/" << q_idx << "|" << files_words.size() << std::endl;
         (*files_words)[queueObj.idx] = stringToMap(queueObj.str, &word_map);
+        merge_queue->push(std::move((*files_words)[queueObj.idx]));
         std::cout << "{" << thread_idx << "}[" << queueObj.idx << "]Calc thread Finish..." << "readEnd:" << readEnd << ", queue.empty():" << file_content_queue.empty() << std::endl;
     }
     std::cout << "Thread {" << thread_idx << "} is dead." << std::endl;
@@ -81,21 +80,47 @@ void read_thread(std::vector<std::string> &&archive_filenames, std::vector<std::
     readEnd = true;
     std::cout << "FILES COUNT:" << q_idx << std::endl;
 }
-//
-//void merge_pair(std::vector<std::unordered_map<std::string, u_int>> *first, std::vector<std::unordered_map<std::string, u_int>> *second){
-//
-//    first = std::accumulate( second->begin(), second->end(), first,
-//                             []( auto &m, auto &p )
-//                             {
-//                                 return ( m[p.first] +=p.second, m );
-//                             } );
-//}
-//
-//void merge_vec(std::vector<std::unordered_map<std::string, u_int>> *vec_map){
-//    for (int i = 0; i < vec_map->size() -1; ++i){
-//        merge_pair(&vec_map[vec_map->size()], &vec_map[i]);
-//    }
-//}
+
+void merge_pair(std::unordered_map<std::string, u_int> &&first, std::unordered_map<std::string, u_int> &&second, queue_t<std::unordered_map<std::string, u_int>> *merge_queue){
+
+    first = std::accumulate( second.begin(), second.end(), first,
+                             []( auto &m, auto &p )
+                             {
+                                 return ( m[p.first] +=p.second, m );
+                             });
+    merge_queue->push(std::move(first));
+}
+
+void merge_thread(queue_t<std::unordered_map<std::string, u_int>> *merge_queue, int i, int *working_threads, std::mutex *mu){
+    while(true){
+        std::cout << "Merge queue size:" << merge_queue->size() << std::endl;
+        std::cout << "Working threads:" << *working_threads << std::endl;
+        if (*working_threads == 1 && merge_queue->size() == 1)
+            break;
+        std::cout << "{" << i << "}" << " Starts!\n";
+        if (merge_queue->empty())
+            break;
+        std::unordered_map<std::string, u_int> first_to_merge;
+        merge_queue->pop(first_to_merge);
+//        std::cout << "{" << i << "}" << " Pop First!\n";
+        if (merge_queue->empty()){
+            merge_queue->push(std::move(first_to_merge));
+            break;
+        }
+        std::unordered_map<std::string, u_int> second_to_merge;
+        merge_queue->pop(second_to_merge);
+//        std::cout << "{" << i << "}" << " Pop Second!\n";
+        merge_pair(std::move(first_to_merge), std::move(second_to_merge), merge_queue);
+        std::cout << "Merge queue size:" << merge_queue->size() << std::endl;
+        std::cout << "Working threads:" << *working_threads << std::endl;
+
+    }
+    mu->lock();
+    --(*working_threads);
+    mu->unlock();
+    std::cout << "Thread {" << i << "} is dead." << std::endl;
+}
+
 
 
 
@@ -120,6 +145,32 @@ std::vector<std::string> file_names(std::string path){
     return archive_filenames;
 }
 
+void sort(std::unordered_map<std::string, u_int>& M)
+{
+    std::vector<std::pair<std::string, int> > A;
+
+    for (auto& it : M) {
+        A.emplace_back(it);
+    }
+
+    using pairtype=std::pair<std::string,u_int>;
+    sort(A.begin(), A.end(),
+         [](pairtype a, pairtype b){
+                    return a.second < b.second;
+            }
+        );
+}
+
+template<typename KeyType, typename ValueType>
+std::pair<KeyType,ValueType> get_max( const std::unordered_map<KeyType,ValueType>& x ) {
+    using pairtype=std::pair<KeyType,ValueType>;
+    return *std::max_element(x.begin(), x.end(),
+                             [] (const pairtype & p1, const pairtype & p2) {
+                                        return p1.second < p2.second;
+                                    }
+    );
+}
+
 int main() {
     std::string test_data_folder = "../test";
     std::string full_data_folder = "../data";
@@ -136,10 +187,10 @@ int main() {
     //// 2. Create an vector of words dictionaries, which size if the number of files. (not parallel)
     std::vector<std::unordered_map<std::string, u_int>> files_words(archive_filenames.size());
     //// 3. Calculate words dictionaries for every file. (parallel)
-    // Read archive content
+    queue_t<std::unordered_map<std::string, u_int>> merge_queue;
     std::thread calc_threads[n_calc_threads];
     for(int i = 0; i < n_calc_threads; ++i) {
-        calc_threads[i] = std::thread(calc_thread, &files_words, i);
+        calc_threads[i] = std::thread(calc_thread, &files_words, i, &merge_queue);
     }
 
     std::thread readThread{read_thread, std::move(archive_filenames), &files_words};
@@ -149,12 +200,36 @@ int main() {
     for(int i = 0; i < n_calc_threads; ++i) {
         calc_threads[i].join();
     }
+    std::cout << "ALL THREADS FINISHED!!!\n";
+    std::thread merge_threads[n_merge_threads];
 
-//    std::thread merge_threads[n_merge_threads];
-//    for (int i =0; i < n_merge_threads; ++i) {
+    std::cout << "Merge queue size:" << merge_queue.size() << std::endl;
+
+    int threads_left = n_merge_threads;
+    std::mutex mu;
+    for (int i =0; i < n_merge_threads; ++i) {
+        merge_threads[i] = std::thread(merge_thread, &merge_queue, i, &threads_left, &mu);
+    }
+
+    for (int i =0; i < n_merge_threads; ++i) {
+        merge_threads[i].join();
+    }
+
+    std::unordered_map<std::string, u_int> final_dict;
+
+    merge_queue.pop(final_dict);
+
+    auto max = get_max(final_dict);
+
+    std::cout << max.first << ":" << max.second << std::endl;
+//    sort(final_dict);
 //
+//    for (auto const& [key, val] : final_dict) {
+//        std::cout << key        // string (key)
+//                  << ':'
+//                  << val        // string's value
+//                  << std::endl;
 //    }
-
     auto time_to_calculate = get_current_time_fenced() - before;
     std::cout << "Time: " << to_s(time_to_calculate) << "s" << std::endl;
     std::cout << files_words.size() << std::endl;
--- src/queue_t.h
+++ src/queue_t.h
@@ -12,7 +12,6 @@ private:
     std::queue<T> queue;
     std::mutex mutex;
     std::condition_variable cv;
-    bool is_finished = false;
 public:
     void push(T &&elem){
         std::lock_guard<std::mutex> lock(mutex);
@@ -20,28 +19,17 @@ public:
         queue.push(std::move(elem));
     }
 
-//    bool pop(T& elem){
-//        if (queue.empty())
-//            return false;
-//        std::lock_guard<std::mutex> lock(mutex);
-//        elem = queue.front();
-//        queue.pop();
-//        return true;
-//    }
-
     bool pop(T& elem){
         std::unique_lock<std::mutex> lk(mutex);
-        while (empty() && !is_finished)
+        while (empty())
             cv.wait(lk);
         elem = queue.front();
         queue.pop();
         return true;
     }
 
-    void set_finished() {
-        std::unique_lock<std::mutex> locker(mutex);
-        is_finished = true;
-        cv.notify_all();
+    u_int size(){
+        return queue.size();
     }
 
     bool empty(){
